fae77e9c717d7fda1dca2b1522d6b8abf09835b2
Antoine Duboc <antoinedbc2005@gmail.com>
2025-10-02 12:12:35 +0200

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # Exemple avec un seul exercice
+# Exercice 1 clear, 
 def exercice1():
     print("Exercice 1 : Bonjour le monde !")
     print("Hello World !")
+
+def exercice2():
+    prenom = input ("Entrez votre prénom : ")
+    print(f"bonjour {prenom}!")
 
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
     if choix == "1":
         exercice1()
+    if choix == "2":
+        exercice2()
     else:
         print("Exercice non reconnu.")
         
