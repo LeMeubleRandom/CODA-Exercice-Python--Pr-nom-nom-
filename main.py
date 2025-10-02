1de2711eefcbed34d1fd4dcec8f73a53006c1ff8
Antoine Duboc <antoinedbc2005@gmail.com>
2025-10-02 13:29:43 +0200

    exercice 3

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def exercice2():
     prenom = input ("Entrez votre prénom : ")
     print(f"bonjour {prenom}!")
 
+def exercice3():
+    print("Première ligne\nDeuxième ligne\nTroisième ligne")
+
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
@@ -15,6 +18,8 @@ def main():
         exercice1()
     if choix == "2":
         exercice2()
+    if choix == "3":
+        exercice3()
     else:
         print("Exercice non reconnu.")
         
